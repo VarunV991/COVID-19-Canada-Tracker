626e36a65dd398e1a9b2e5d6715539e50c9f61ee
Varun V <varunofficial99@gmail.com>
2020-09-09 19:41:17 +0530

    Add start script

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 var express = require('express');
 var app = express();
-var cheerio = require('cheerio');
-const request = require('request-promise');
-const fs = require('fs');
 var PORT = process.env.PORT || 3001;
 
 app.use(express.static(__dirname + '/public'));
 app.set("view engine","ejs");
 
 app.get('/',function(req,res){
-        res.render('index')
+    res.render('index')
 })
 
-app.listen(PORT ,process.env.IP,function(){
-    console.log("COVID tracker has started");
-})
+app.listen(process.env.PORT || 3001);
